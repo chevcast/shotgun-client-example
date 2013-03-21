5cb4af4ffd2d20f0da2fdd328678a95c07767614
Alex Ford <Alex@CodeTunnel.com>
2013-03-20 21:48:32 -0600

    Initial commit.

---
+++ app.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , shotgun = require('shotgun')
+  , shotgunClient = require('shotgun-client');
+
+var app = express(),
+    shell = new shotgun.Shell();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+shotgunClient.attach(server, shell);
