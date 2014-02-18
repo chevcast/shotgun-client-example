549a5fa4db04c831a124832c3492f0356048e087
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-02-18 14:14:21 -0700

    Updated node_modules.

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 $(function () {
+    // JQuery shells
     $('#shell1').shotgunConsole({ namespace: 'shell1' }).execute('intro');
     $('#shell2').shotgunConsole({ namespace: 'shell2' }).execute('intro');
-    $('#shell3').shotgunConsole({ namespace: 'shell3' }).execute('intro');
-    $('#shell4').shotgunConsole({ namespace: 'shell4' }).execute('intro');
+
+    // Angular shells are found in the markup.
 });
